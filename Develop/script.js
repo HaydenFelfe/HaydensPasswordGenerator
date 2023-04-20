a38261c013f40a16fbd61e94a8f26fd14f8fc64c
haydenfelfe <felfehayden@gmail.com>
2023-04-20 12:34:46 -0700

    Added in the if statement that allows users to add in a password length bettwen 8 and 128 characters. If they enter anything outside the parameters then they are given an error message.

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,16 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function generatePassword() {
     var passwordLength = prompt("How many characters would you like your password to contain?");
-
+    if (isNaN(passwordLength)) {
+        window.alert("Please enter a valid number");
+        return;
+      } else if (passwordLength < 8) {
+        window.alert("Your password must be at least 8 characters.");
+        return;
+      } else if (passwordLength > 128) {
+        window.alert("Your password must be less than 128 characters.");
+        return;
+      }
 
 
 }
